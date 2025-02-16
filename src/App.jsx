34e4d31ce6a88eabf4e5d9bdfc3197fc5b2e4b64
Krits-devops <kritagyarm9@gmail.com>
2025-02-15 23:59:23 -0500

    Added AdminDashboard
    Added AdminDashboard component and updated TaskList component with multiple task cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Login from './components/Auth/Login';
 import EmployeeDashboard from './components/Dashboard.jsx/EmployeeDashboard';
+import AdminDashboard from './components/Dashboard.jsx/AdminDashboard';
 
 export const App = () => {
   return (
     <>
       {/* <Login/> */}
-      <EmployeeDashboard/>
+      {/* <EmployeeDashboard/> */}
+      <AdminDashboard/>
     </>
   )
 }
